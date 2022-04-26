6d00c8c3838c374849b95ad90a542e8ff1d1a00f
Jasmineleigh <89361875+Jasmineleigh@users.noreply.github.com>
2022-04-26 10:21:18 -0400

    Update Main.java
    Added another graph for another example of the algorithm working, and reformatted the print out.

--- Main.java
+++ Main.java
@@ -1,8 +1,64 @@
 package finalProject;
 
 public class Main {
-	public static void main(String[] args) {
-		//add nodes
+		
+	public static void main(String[]args) {
+		InverseKruskal_Letters();
+		InverseKruskal_ValdostaCampus();
+		
+	}
+	
+	private static void InverseKruskal_Letters() {
+		Node a = new Node ("A", 0);
+		Node b = new Node ("B", 1);
+		Node c = new Node ("C", 2);
+		Node d = new Node ("D", 3);
+		Node E = new Node ("E", 4);
+		Node f = new Node ("F", 5); 
+		Node g = new Node ("G", 6);
+		
+		Edge ab = new Edge(a, b, 7);
+		Edge ad = new Edge(a, d, 5);
+		Edge bc = new Edge(b, c, 8);
+		Edge be = new Edge(b, E, 7);
+		Edge bd = new Edge(b, d, 9);
+		Edge ce = new Edge(c, E, 5);
+		Edge de = new Edge(d, E, 15);
+		Edge df = new Edge(d, f, 6);
+		Edge ef = new Edge(E, f, 8);
+		Edge eg = new Edge(E, g, 9);
+		Edge fg = new Edge(f, g, 11);
+		
+		Graph graph = new Graph(7);
+		graph.addEdge(ab);
+		graph.addEdge(ad);
+		graph.addEdge(bc);
+		graph.addEdge(be);
+		graph.addEdge(bd);
+		graph.addEdge(ce);
+		graph.addEdge(de);
+		graph.addEdge(df);
+		graph.addEdge(ef);
+		graph.addEdge(eg);	
+		graph.addEdge(fg);
+		
+		// Print Original edges
+		System.out.println("Original edges in graph:\n\n" + graph);
+		
+		// Print list of edges in MST
+		System.out.println("Edges in MST:\n\n" + graph.printMST());
+		System.out.println("Edges removed from graph:\n");
+		
+		for(Edge e: graph.getRemovedEdges()) {
+			System.out.println("\t" + e);
+		}
+		
+		System.out.println();
+	}
+	
+	private static void InverseKruskal_ValdostaCampus() {
+		
+		// Make nodes
 		Node pe = new Node("PE Complex", 0);
 		Node edu = new Node("Education Building", 1);
 		Node oak = new Node("Oak Parking", 2);
@@ -22,85 +78,40 @@ public class Main {
 		Node reade = new Node("Reade", 16);
 		Node bailey = new Node("Bailey Science Center", 17);
 		Node ga = new Node("Georgia", 18);
-		Node pow = new Node("Powell Hall", 1);
+		Node pow = new Node("Powell Hall", 19);
 		
-		//add edges
+		//Make edges
 		Edge e1 = new Edge(pe, edu, 770);
-		pe.addEdge(e1);
-		
 		Edge e2 = new Edge(edu, oak, 300);
 		Edge e3 = new Edge(edu, union, 480);
-		edu.addEdge(e2);
-		edu.addEdge(e3);
-		
 		Edge e4 = new Edge(oak, arts, 400);
-		oak.addEdge(e4);
-		
 		Edge e5 = new Edge(arts, odum, 422);
-		arts.addEdge(e5);
-		
 		Edge e6 = new Edge(lowndes, odum, 290);
 		Edge e7 = new Edge(lowndes, patt, 248);
-		lowndes.addEdge(e6);
-		lowndes.addEdge(e7);
-		
 		Edge e8 = new Edge(patt, brown, 129);
-		patt.addEdge(e8);
-		
 		Edge e9 = new Edge(brown, odum, 310);
-		brown.addEdge(e9);
-		
 		Edge e10 = new Edge(odum, union, 212);
 		Edge e11 = new Edge(odum, conv, 360);
 		Edge e12 = new Edge(odum, nev, 260);
-		odum.addEdge(e10);
-		odum.addEdge(e11);
-		odum.addEdge(e12);
-		
 		Edge e13 = new Edge(union, hopp, 216);
-		union.addEdge(e13);
-		
 		Edge e14 = new Edge(hopp, palms, 139);
-		hopp.addEdge(e14);
-		
 		Edge e15 = new Edge(conv, ashley, 279);
 		Edge e16 = new Edge(conv, nev, 108);
-		conv.addEdge(e15);
-		conv.addEdge(e16);
-		
 		Edge e17 = new Edge(nev, west, 93);
-		nev.addEdge(e17);
-		
 		Edge e18 = new Edge(palms, lang, 161);
 		Edge e19 = new Edge(palms, ashley, 244);
-		palms.addEdge(e18);
-		palms.addEdge(e19);
-		
 		Edge e20 = new Edge(ashley, lang, 208);
 		Edge e21 = new Edge(ashley, reade, 181);
 		Edge e22 = new Edge(ashley, west, 109);
-		ashley.addEdge(e20);
-		ashley.addEdge(e21);
-		ashley.addEdge(e22);
-		
 		Edge e23 = new Edge(west, bailey, 139);
-		west.addEdge(e23);
-		
 		Edge e24 = new Edge(lang, ga, 192);
-		lang.addEdge(e24);
-		
 		Edge e25 = new Edge(reade, ga, 157);
 		Edge e26 = new Edge(reade, bailey, 96);
-		reade.addEdge(e25);
-		reade.addEdge(e26);
-		
 		Edge e27 = new Edge(bailey, pow, 91);
-		bailey.addEdge(e27);
-		
 		Edge e28 = new Edge(ga, pow, 232);
-		ga.addEdge(e28);
 		
-		Graph graph = new Graph(28);
+		// Add Edges to Graph
+		Graph graph = new Graph(20);
 		graph.addEdge(e1);
 		graph.addEdge(e2);
 		graph.addEdge(e3);
@@ -130,6 +141,18 @@ public class Main {
 		graph.addEdge(e27);
 		graph.addEdge(e28);
 		
-		System.out.println(graph);
+		// Print Original edges
+		System.out.println("Original edges in graph:\n\n" + graph);
+		
+		// Print list of edges in MST
+		System.out.println("Edges in MST:\n\n" + graph.printMST());
+		System.out.println("Edges removed from graph:\n");
+		
+		for(Edge e: graph.getRemovedEdges()) {
+			System.out.println("\t" + e);
+		}
+		
+		System.out.println();
 	}
+	
 }
