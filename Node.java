ac57252c8001a1567cc9e5956b10770befc988fc
Jasmineleigh <89361875+Jasmineleigh@users.noreply.github.com>
2022-04-26 22:06:40 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -7,7 +7,6 @@ import java.util.List;
 public class Node {
 	String name;
 	int index;
-	List<Edge> edges = new ArrayList<>();
 	
 	public Node(String name, int index) {
 		this.name = name;
@@ -21,50 +20,10 @@ public class Node {
 	public String getName() {
 		return name;
 	}
-	
-	public List getEdges() {
-		return edges;
+
+	@Override
+	public String toString() {
+		return name + ": " + index;
 	}
-	
-	public boolean addEdge(Edge e) {
-		
-		if(e.getStart().equals(this.name)) {
-			if(!edges.contains(e)) {
-				edges.add(e);
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public int getNumEdges() {
-		return edges.size();
-	}
-	
-	public Edge removeEdge(Edge e) {
-		Edge removed = null;
-		if(!edges.contains(e)) {
-			removed = e;
-			edges.remove(e);
-		}
-		return removed;
-	}
-	
-	public List<Edge> getSortedEdges(){
-		
-		if(edges.size() == 1)
-			return edges;
-		else {
-	
-			for(int i = 1; i < edges.size(); i++) {
-				if(edges.get(i).getWeight() > edges.get(i-1).getWeight()) {
-					Collections.swap(edges, i-1, i);
-				}
-				
-			}
-			return edges;
-		}
-	}
-	
+
 }
